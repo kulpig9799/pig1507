441c63c4f43a6a260bb3cd31454c12c030547617
kulpig9799 <nguyenhoangcong152@gmail.com>
2019-07-07 23:49:14 +0700

    Update java.js

--- java.js
+++ java.js
@@ -1,4 +1,10 @@
-var images = ["img/Pig2.jpg","img/Pig3.jpg","img/Pig4.jpg","img/Pig5.jpg,"img/Pig6.jpg","img/Pig7.jpg"]
+
+var images = ["img/Pig2.jpg","img/Pig3.jpg","img/Pig4.jpg","img/Pig5.jpg","img/Pig6.jpg","img/Pig7.jpg","img/Pig8.jpg","img/Pig9.jpg","img/Pig10.jpg","img/Pig11.jpg","img/Pig12.jpg","img/Pig13.jpg",
+"img/Pig14.jpg","img/Pig15.jpg","img/Pig16.jpg","img/Pig17.jpg","img/Pig18.jpg","img/Pig19.jpg","img/Pig20.jpg","img/Pig21.jpg","img/Pig22.jpg","img/Pig23.jpg","img/Pig24.jpg","img/Pig25.jpg","img/Pig26.jpg",
+"img/Pig27.jpg","img/Pig28.jpg","img/Pig29.jpg","img/Pig30.jpg","img/Pig31.jpg","img/Pig32.jpg","img/Pig33.jpg","img/Pig34.jpg","img/Pig35.jpg","img/Pig36.jpg","img/Pig37.jpg","img/Pig38.jpg","img/Pig39.jpg",
+"img/Pig40.jpg","img/Pig41.jpg","img/Pig42.jpg","img/Pig43.jpg","img/Pig44.jpg","img/Pig45.jpg","img/Pig46.jpg","img/Pig47.jpg","img/Pig48.jpg","img/Pig49.jpg","img/Pig50.jpg","img/Pig51.jpg","img/Pig52.jpg","img/Pig53.jpg",
+"img/Pig54.jpg","img/Pig55.jpg"]
+
 
 for ( i = 0; i < images.length; i++ ){
   $(".slideshow-container").append(`
